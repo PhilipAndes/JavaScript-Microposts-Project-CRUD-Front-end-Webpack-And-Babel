a792e75d8e77fc635a4e7d27ca5fd1dda4437a7c
Philip Andes <philip__andes@hotmail.nl>
2019-05-21 12:44:44 +0200

    Changed package.json settings and run npm build

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ module.exports = {
         exclude: /node_modules/,
         loader: 'babel-loader',
         query: {
-           presets: ['@babel/preset-env', '@babel/preset-stage-0']
+           presets: ['@babel/preset-env']
         }
     }]
   }
